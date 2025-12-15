b7a6c751b1e24d6675674f22625c4cbf92ed63a8
beejh02 <joon0448@naver.com>
2025-12-16 02:51:08 +0900

    add datasets4

--- sort_datasetsImage.py
+++ sort_datasetsImage.py
@@ -54,4 +54,4 @@ def rename_images_in_directory(root_dir, start_number=1):
 
 # 실행 예시
 if __name__ == "__main__":
-    rename_images_in_directory("./datasets/images/temp", start_number=157)
+    rename_images_in_directory("./datasets/images/temp", start_number=179)
